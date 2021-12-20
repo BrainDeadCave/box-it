91e80af2544d799e0eee6063e721513309c84162
Dale <djs93@njit.edu>
2021-12-19 21:29:37 -0500

    Camera dragging and scene color changes

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,10 +12,66 @@ public class CamControl : MonoBehaviour
     /// <summary>
     /// When moving the mouse, drag the camera around 
     /// </summary>
-    public void OnMouseDrag(InputAction.CallbackContext context)
+    [SerializeField]
+    private InputAction mouseClick;
+    [SerializeField]
+    private float upperBound;
+    private Vector3 upperBoundPos;
+    [SerializeField]
+    private float lowerBound;
+    private Vector3 lowerBoundPos;
+
+    private Camera mainCamera;
+    private WaitForFixedUpdate waitForFixedUpdate = new WaitForFixedUpdate();
+    private float oldY;
+
+	private void Awake()
+	{
+        mainCamera = Camera.main;
+        upperBoundPos = mainCamera.transform.position;
+        upperBoundPos.y = upperBound;
+        lowerBoundPos = mainCamera.transform.position;
+        lowerBoundPos.y = lowerBound;
+    }
+
+	private void OnEnable()
+	{
+        mouseClick.Enable();
+        mouseClick.performed += MousePressed;
+    }
+    private void OnDisable()
     {
-        Vector3 newPos = transform.position;
-        newPos.y += context.ReadValue<Vector2>().y;
-        transform.position = newPos;
+        mouseClick.performed -= MousePressed;
+        mouseClick.Disable();
+    }
+    private void MousePressed(InputAction.CallbackContext context)
+	{
+        oldY = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()).y;
+        StartCoroutine(DragUpdate());
+    }
+
+	private IEnumerator DragUpdate()
+	{
+        while(mouseClick.ReadValue<float>() != 0)
+		{
+            float newY = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()).y;
+            float difference = newY - oldY;
+            Vector3 camPos = mainCamera.transform.position;
+            camPos.y -= difference;
+            if(camPos.y > upperBound)
+			{
+                mainCamera.transform.position = upperBoundPos;
+            }
+            else if (camPos.y < lowerBound)
+            {
+                mainCamera.transform.position = lowerBoundPos;
+            }
+			else
+			{
+                mainCamera.transform.position = camPos;
+            }
+            oldY = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()).y;
+            yield return waitForFixedUpdate;
+        }
     }
 }
